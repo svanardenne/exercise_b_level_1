0685aa47058d1b6f537de1223d1fd2bdf2e031b0
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-07-04 09:32:09 -0700

    Added presence validations and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :bio, presence: true
   validates_uniqueness_of :id
+  validates_uniqueness_of :username
+  validates :email,
+            format: { uniqueness: { case_sensitive: false } }
 end
