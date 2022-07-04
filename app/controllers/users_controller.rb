df6837f0b1153d0b856f6921d885e9f2dd86a262
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-07-04 10:27:43 -0700

    Added comments to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  # Returns user details
   def show; end
 
+  # Returns universally unique identifier
   def show_id; end
 
+  # Creates user
   def create
     @user = User.new(username: 'Shaun',
                      email: 's.vanardenne@gmail.com',
