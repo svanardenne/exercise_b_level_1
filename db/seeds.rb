668bb9b926de93e7b10a1a65a5c49903c197a2b0
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-07-05 20:30:23 -0700

    Changed back to sqlite for exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.create(id: '37de88e1-0488-4d1a-b091-23c674cd0451',
-            username: Faker::Internet.unique.username,
-            email: Faker::Internet.unique.email,
-            bio: Faker::Lorem.paragraph)
+50.times do
+  User.create(username: Faker::Internet.unique.username,
+              email: Faker::Internet.unique.email,
+              bio: Faker::Lorem.paragraph)
+end
