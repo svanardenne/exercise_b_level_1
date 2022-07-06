bcff16d626e96585e7bbee19ce9b3fc5b3dc4e8e
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-07-05 22:37:50 -0700

    Added endpoint tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should get id' do
+    get id_path
+    assert_response :success
+  end
+
+  test 'should get user' do
+    get 'http://localhost:3000/3d8b3ed5-fb80-43d2-a719-4b9c6941980f'
+    assert_response :success
+  end
+
+  test 'should create user' do
+    assert_difference 'User.count' do
+      post create_user_path
+    end
+    assert_response :success
+  end
 end
